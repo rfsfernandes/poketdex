b1883e32306c7b281e4e888f1e572cb327d3a5d9
Rodrigo Fernandes <rfernandes@digital-works.com>
2021-01-20 16:22:48 +0000

    fixed textSize to ssp

--- PoketDex/app/src/main/java/pt/rfsfernandes/ui/activities/SplashActivity.java
+++ PoketDex/app/src/main/java/pt/rfsfernandes/ui/activities/SplashActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import pt.rfsfernandes.R;
 
-public class SplaceActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splace);
+    setContentView(R.layout.activity_splash);
   }
 }
