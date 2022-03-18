9caea826e8365743b656b2ad89edb483fe3e1e18
Rodrigo Fernandes <rodrigo.fernandes@appsfactory.de>
2022-03-18 14:33:34 +0100

    Removed Java version from main branch

--- PoketDex/app/src/main/java/pt/rfsfernandes/custom/callbacks/ItemListClicked.java
+++
@@ -1,9 +0,0 @@
-package pt.rfsfernandes.custom.callbacks;
-
-/**
- * Interface ItemListClicked created at 1/16/21 20:30 for the project PoketDex
- * By: rodrigofernandes
- */
-public interface ItemListClicked<T> {
-  void onClick(T object);
-}
--- PoketDex/app/src/main/java/pt/rfsfernandes/data/local/DBContract.java
+++
@@ -1,6 +0,0 @@
-package pt.rfsfernandes.data.local;
-
-class DBContract {
-  static final int DATABASE_VERSION = 1;
-  static final String DATABASE_NAME = "db-pokedex-app";
-}
